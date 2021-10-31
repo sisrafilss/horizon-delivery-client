818a97dd53dc6cdf95bddaa8d7a5452d1960447b
Israfil <sisrafilss@gmail.com>
2021-10-31 09:37:18 +0600

    banner updated

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,47 +1,62 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel'
-import banner1 from '../../../images/banner/banner-1.jpg';
-import banner2 from '../../../images/banner/banner-2.jpg';
-import banner3 from '../../../images/banner/banner-3.jpg';
+import Carousel from "react-bootstrap/Carousel";
+import "./Banner.css";
+import banner1 from "../../../images/banner/banner1.jpg";
+import banner2 from "../../../images/banner/banner2.jpg";
+import banner3 from "../../../images/banner/banner3.jpg";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={banner1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={banner2}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={banner3}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        <img className="d-block w-100" src={banner1} alt="First slide" />
+        <Carousel.Caption className="slide-caption">
+          <h1 className="slide-title p-3 d-inline-block fw-bold display-2 rounded mb-0">
+            Global Shipping through Asia-Pas, Any Beyond
+          </h1>
+          <br />
+          <p className="slide-title-2 p-2 my-0 d-inline-block rounded">
+            Lorem ipsum dolor sit amet consectetur.
           </p>
+          <br />
+          <Link to="/services" className="btn btn-info mt-0">
+            View Services &nbsp; <i className="fas fa-plus"></i>
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={banner2} alt="Second slide" />
+
+        <Carousel.Caption className="slide-caption">
+          <h1 className="slide-title p-3 d-inline-block fw-bold display-2 rounded mb-0">
+            Global Shipping through Asia-Pas, Any Beyond
+          </h1>
+          <br />
+          <p className="slide-title-2 p-2 my-0 d-inline-block rounded">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+          <br />
+          <Link to="/services" className="btn btn-info mt-0">
+            View Services &nbsp; <i className="fas fa-plus"></i>
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={banner3} alt="Third slide" />
+
+        <Carousel.Caption className="slide-caption">
+          <h1 className="slide-title p-3 d-inline-block fw-bold display-2 rounded mb-0">
+            Global Shipping through Asia-Pas, Any Beyond
+          </h1>
+          <br />
+          <p className="slide-title-2 p-2 my-0 d-inline-block rounded">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+          <br />
+          <Link to="/services" className="btn btn-info mt-0">
+            View Services &nbsp; <i className="fas fa-plus"></i>
+          </Link>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
